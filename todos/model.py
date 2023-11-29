11a3906600a49ecaca8ecf6c3167ba005706e0b0
limstonestone <ehus1118@gmail.com>
2023-11-29 18:52:06 +0900

    Chap4) Templating

--- todos/model.py
+++ todos/model.py
@@ -1,24 +1,33 @@
+from typing import List, Optional
+
+from fastapi import Form
 from pydantic import BaseModel
-from typing import List
 
 
 class Todo(BaseModel):
-    id: int
+    id: Optional[int]
     item: str
+
+    @classmethod
+    def as_form(cls, item: str = Form(...)):
+        return cls(item=item)
+
+    class Config:
+        json_schema_extra = {"example": {"id": 1, "item": "Example schema!"}}
 
 
 class TodoItem(BaseModel):
     item: str
 
     class Config:
-        schema_extra = {"example": {"item": "Read the next chapter of the book."}}
+        json_schema_extra = {"example": {"item": "Read the next chapter of the book"}}
 
 
 class TodoItems(BaseModel):
     todos: List[TodoItem]
 
     class Config:
-        schema_extra = {
+        json_schema_extra = {
             "example": {
                 "todos": [{"item": "Example schema 1!"}, {"item": "Example schema 2!"}]
             }
--- todos/todo.py
+++ todos/todo.py
@@ -1,60 +1,73 @@
-from fastapi import APIRouter, Path, HTTPException, status
+from fastapi import APIRouter, Path, HTTPException, status, Request, Depends
+from fastapi.templating import Jinja2Templates
+
 from model import Todo, TodoItem, TodoItems
 
 todo_router = APIRouter()
 
 todo_list = []
 
+templates = Jinja2Templates(directory="templates/")
 
-@todo_router.post("/todo", status_code=201)
-async def add_todo(todo: Todo) -> dict:
+
+@todo_router.post("/todo")
+async def add_todo(request: Request, todo: Todo = Depends(Todo.as_form)):
+    todo.id = len(todo_list) + 1
     todo_list.append(todo)
-    return {"message": "Todo added successfully."}
+    return templates.TemplateResponse(
+        "todo.html", {"request": request, "todos": todo_list}
+    )
 
 
 @todo_router.get("/todo", response_model=TodoItems)
-async def retrieve_todos() -> dict:
-    return {"todos": todo_list}
+async def retrieve_todo(request: Request):
+    return templates.TemplateResponse(
+        "todo.html", {"request": request, "todos": todo_list}
+    )
 
 
 @todo_router.get("/todo/{todo_id}")
 async def get_single_todo(
-    todo_id: int = Path(..., title="The ID of the todo to retrieve.")
-) -> dict:
+    request: Request, todo_id: int = Path(..., title="The ID of the todo to retrieve.")
+):
     for todo in todo_list:
         if todo.id == todo_id:
-            return {"todo": todo}
-    return HTTPException(
+            return templates.TemplateResponse(
+                "todo.html", {"request": request, "todo": todo}
+            )
+    raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail="Todo with supplied ID doesn't exist.",
+        detail="Todo with supplied ID doesn't exist",
     )
 
 
 @todo_router.put("/todo/{todo_id}")
 async def update_todo(
+    request: Request,
     todo_data: TodoItem,
-    todo_id: int = Path(..., title="The ID of the todo to be updated"),
+    todo_id: int = Path(..., title="The ID of the todo to be updated."),
 ) -> dict:
     for todo in todo_list:
         if todo.id == todo_id:
             todo.item = todo_data.item
             return {"message": "Todo updated successfully."}
-    return HTTPException(
+
+    raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail="Todo with supplied ID doesn't exist.",
+        detail="Todo with supplied ID doesn't exist",
     )
 
 
 @todo_router.delete("/todo/{todo_id}")
-async def delete_single_todo(todo_id: int) -> dict:
+async def delete_single_todo(request: Request, todo_id: int) -> dict:
     for index in range(len(todo_list)):
         todo = todo_list[index]
         if todo.id == todo_id:
             todo_list.pop(index)
             return {"message": "Todo deleted successfully."}
-    return HTTPException(
+    raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail="Todo with supplied ID doesn't exist.",
+        detail="Todo with supplied ID doesn't exist",
     )
 
 
